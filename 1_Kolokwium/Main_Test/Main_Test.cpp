a324b52d609baa5239e5cb5f00415d0f63f82117
T3ngll <98605712+T3ngll@users.noreply.github.com>
2022-01-30 19:00:13 +0100

    Check 1Kolok Main Test

--- 1_Kolokwium/Main_Test/Main_Test.cpp
+++ 1_Kolokwium/Main_Test/Main_Test.cpp
@@ -2,24 +2,79 @@
 using namespace std;
 int main()
 {
-	int arr[50];
-	cout << "Write elements: ";
-	for (int i = 0; i < 50; i++)
-	{
-		cin >> arr[i];
-		if (arr[i] == 0)
-		{
-			break;
-		}
-	}
-	cout << "Elements: ";
-	for (int i = 0; arr[i] !=0 ; i++)
-	{
-		cout<< arr[i]<<" ";
-	}
-	
+    /*int checker, max;
+    checker = max = 0;
+    int arr[50];
+    cout << "Write elements: ";
+    for (int i = 0; i < 50; i++, max++)
+    {
+        cin >> arr[i];
+        if (arr[i] == 0) break;
+    }
+    for (int i = 2; i < max; i++)
+    {
+        int checker, checker2, sum, sum2;
+        checker = checker2 = sum = sum2 = 0;
+        sum = arr[i] + arr[i - 1];
+        for (int i = 1; i <= sum; i++)
+            if (sum % i == 0)  checker++;
+        sum2 = arr[i - 1] + arr[i - 2];
+        for (int i = 1; i <= sum2; i++)
+            if (sum2 % i == 0)  checker2++;
+        if (checker2 > checker)
+        {
+            cout << arr[i - 1] << ", " << arr[i] << "\t";
+        }
 
-
-
-	return 0;
+    }*/
+    int size;
+    cout << "Write size (>0): ";
+    cin >> size;
+    if (size <= 0)
+    {
+        for (; size <= 0;)
+        {
+            cout << "Wtite correct size(>0): ";
+            cin >> size;
+        }
+    }
+    int* arr = new int[size];
+    cout << "Write elements(>0): ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] <= 0)
+        {
+            for (; arr[i] <= 0;)
+            {
+                cout << "Wtite correct element(>0): ";
+                cin >> arr[i];
+            }
+        }
+    }
+    cout << "Elements: ";
+    int min = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        cout << arr[i] << " ";
+    }
+    for (int i = 1; i + 3 < size; i + 3)
+    {
+        int max = arr[i - 1];
+        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) max = arr[i];
+        if (arr[i+1] >= arr[i - 1] && arr[i+1] >= arr[i]) max = arr[i+1];
+        if (arr[i-1] = max) arr[i-1] = arr[i-1] % min;
+        if (arr[i] = max)   arr[i] = arr[i] % min;
+        if (arr[i+1] = max) arr[i+1] = arr[i+1] % min;
+    }
+    cout << "Elements after function: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
